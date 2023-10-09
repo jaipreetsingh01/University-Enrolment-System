635c0fe85a272b7eab17bd4b2a0c418075ef5100
jaipreetsingh01 <jaipreetsingh01@gmail.com>
2023-10-10 08:40:55 +1100

    Added In.java &  Shell code for University Class

--- src/Student.java
+++ src/Student.java
@@ -1,11 +1,31 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Student {
 
     private String name;
     private String email;
-    private char[] password;
+    private String password;
     private int studentID;
     private List<Subject> enrolledSubjects;
 
+    public Student(String name, String email, String password, int studentID) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.studentID = studentID;
+        this.enrolledSubjects = new ArrayList<>();
+    }
+
+    public void enrolSubject() {
+
+    }
+
+    public void withdrawSubject() {
+
+    }
+
+    public void viewEnrollment() {
+
+    }
 }
--- src/University.java
+++ src/University.java
@@ -10,6 +10,82 @@ public class University {
     }
 
     public University() {
+    } // Contructor
+
+    private void registerStudentData() {
+        // verify against data & verify pattern
+        // add student through contrcutor
     }
 
+    private void generateID() {
+    }
+
+    private void saveStudentData() {
+    }
+
+    private boolean verifyCredentials(String email, String password) {
+        return true; // FOR NOW
+    }
+
+    private void updatePassword(String newPass) {
+    }
+
+    private void updateStudentData() {
+
+    }
+
+    private void deleteStudentData() {
+
+    }
+
+    private void deleteAllStudentData() {
+
+    }
+
+    private char readChoice() {
+        System.out.print("University System: (A)dmin, (S)tudent or X");
+        return In.nextChar();
+    }
+
+    private String readEmail() {
+        System.out.print("Email: ");
+        return In.nextLine();
+        // verify pattern & return error if does not patch
+    }
+
+    private String readPassword() {
+        System.out.print("Password: ");
+        return In.nextLine();
+        // verify pattern & return error if does not patch
+    }
+
+    private Student findStudent() {
+        // returns student after matching
+    }
+
+    private void studentLogin() {
+
+    }
+
+    private void adminLogin() {
+    }
+
+    public void displayMenu() {
+        System.out.println();
+        char c;
+        while ((c = readChoice()) != 'X') {
+            switch (c) {
+                case 'A':
+                    adminLogin();
+                    break;
+                case 'S':
+                    studentLogin();
+                    break;
+                default:
+                    // Help menu
+                    break;
+            }
+
+        }
+    }
 }
